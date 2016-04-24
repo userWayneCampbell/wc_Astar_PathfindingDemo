652cb127852f0461ce22e64f171f4bec9b67e7e9
WilliBG18 <b-gray.1@onu.edu>
2016-04-24 16:26:11 -0400

    Revert "Added map cost"
    This reverts commit 81d757861a37b45594adc5e2da3e7b43d1b38e85.

--- Scripts/AStar.js
+++ Scripts/AStar.js
@@ -163,7 +163,6 @@
           "y": y,
           "state": OPENED,
           "cost": parent.cost + wc_findCost(x, y),
-          "mapCost": parent.cost + bg_findMapCost(x, y),
           "parent": parent,
           "depth": parent.depth + 1
         };
@@ -211,7 +210,6 @@
     results("Explored <b>" + visitCount + "</b> grid squares (<b>" + (height * width - visitCount) + "</b> unexplored).<br>");
     results("Path: <b>" + (count) + "</b> grid squares.<br>");
     results("Cost: <b>" + (node.cost) + "</b>.<br>");
-    results("Map Cost: <b>" + (node.cost) + "</b>.<br>");
     results("The deepest node visited had depth <b>" + deepest + "</b>.<br>");
     results("The search took <b>" + ((endTime.getTime() - startTime.getTime()) / 1000) + "</b> seconds.<br>")
     results("path: " + p + "<br>");
@@ -269,45 +267,6 @@
     return c;
   }
 
-function bg_findMapCost(x, y, astar) {
-    if (x < 0 || x > width - 1 || y < 0 || y > height - 1) {
-      return 0
-    };
-    var c;
-    var node = GraphSearch[x][y];
-
-    var gx = Number(goal[0]);
-    var gy = Number(goal[1]);
-
-		var classType = getClassType(classTypeObject);
-
-    switch (node.feature) {
-      case "R":
-        c = classType.R;
-        break;
-      case "f":
-        c = classType.f;
-        break;
-      case "F":
-        c = classType.F;
-        break;
-      case "h":
-        c = classType.h;
-        break;
-      case "r":
-        c = classType.r;
-        break;
-      case "M":
-        c = classType.M;
-        break;
-		  case "W":
-	      c = classType.W;
-	      break;
-      default:
-        c = 999999;
-    }
-    return c;
-    }
   //Total Number of nodes visited
   function countVisited(GraphSearch) {
     var count = 0;
